08cdf14d356ecd9d838838ad88abf4abbcb248ee
전여진 <jeon-yeojin@jeon-yeojin-ui-MacBookAir.local>
2023-06-28 16:27:08 +0900

    [test] Controller의 반환타입이 Thymeleaf의 html명과 mapping 되는 이유 -- cf. ThymeleafProperties

--- src/main/java/tacos/HomeController.java
+++ src/main/java/tacos/HomeController.java
@@ -8,6 +8,8 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(){
+//        return 값이 html 파일명을 지정 (브라우에 나타나는 뷰의 논리적인 이름)
+//        cf. p45 (ThymeleafProperties.class)
         return "home";
     }
 }
