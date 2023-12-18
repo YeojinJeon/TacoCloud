3117d58c90120c2d9e175fa335f5b00d6331f948
yeojin <jeonyeojin1993@gmail.com>
2023-12-18 17:55:34 +0900

    [test] 유효성 검사 임시 주석 -- 빠른 개발 진행을 위해 엄격한 유효성 검사 임시 주석

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -25,14 +25,13 @@ public class Order {
     @NotBlank(message = "Zip code is required")
     private String deliveryZip;
 
-    @CreditCardNumber(message = "Not a valid credit card number")
+//    @CreditCardNumber(message = "Not a valid credit card number")
     private String ccNumber;
 
-    @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
-            message = "Must be formatted MM/YY")
+//    @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$", message = "Must be formatted MM/YY")
     private String ccExpiration;
 
-    @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
+//    @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
 
 }
