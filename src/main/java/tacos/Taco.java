0d42571c7cb5fda137db10e7d55e036274e92a5a
yeojin <jeonyeojin1993@gmail.com>
2023-12-18 18:07:30 +0900

    [feat] 퍼시스턴스를 곻려한 도메인 객체 수정하기 -- 객체를 데이터베이스에 저장하고자 할 때는 해당 객체를 고유하게 식별해 주는 필드를 하나 추가하는 것이 좋다.

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -5,12 +5,17 @@ import lombok.Data;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.List;
+import java.util.Date;
 
 /*
  * 타코 디자인을 정의하는 도메인 객체
  */
 @Data
 public class Taco {
+
+    //퍼시스턴트를 고려한 도메인 객체 수정
+    private Long id;
+    private Date createdAt;
 
     @NotNull
     @Size(min = 5, message = "Name must be at least 5 characters long")
