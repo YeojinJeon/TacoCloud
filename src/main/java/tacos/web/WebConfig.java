7096d36bef4bacb6a74af633bba9bc44e4b88982
yeojin <jeonyeojin1993@gmail.com>
2023-12-18 17:57:18 +0900

    [feat] View Controller 선언 : WebMvcConfigurer -- ViewController 인 HomeController를 대체하는 간단한 ViewController 생성 -- 중요한 것은 WebMvcConfigurer 인터페이스를 구현한다는 것.

--- src/main/java/tacos/web/WebConfig.java
+++ src/main/java/tacos/web/WebConfig.java
@@ -1,2 +1,15 @@
-package tacos.web;public class WebConfig {
+package tacos.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/").setViewName("home");
+
+    }
 }
